9f66524fc3ad3c4f2fdc59fbfcc5e6918b6496b0
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2024-01-10 10:08:41 +0100

    Vernieuwde versie

--- main.go
+++ main.go
@@ -1,55 +1,49 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	for {
-		var spaargeld float64
-		var gepland float64
-		var leefgeld float64
-		var maandloon float64
-		var rechtstreeks float64
-		var jaar float64
-		var maand float64
-		var maandsparen float64
-		fmt.Print("\n\nHoeveel spaargeld wil je (+++): €")
-		fmt.Scanln(&spaargeld)
-		fmt.Print("Geplande aankopen (++): €")
-		fmt.Scanln(&gepland)
-		fmt.Print("Hoeveel leefgeld per maand heb je nodig: €")
-		fmt.Scanln(&leefgeld)
-		fmt.Print("Bedrag dat je rechtstreeks op je spaarrekening zet: €")
-		fmt.Scanln(&rechtstreeks)
-		fmt.Print("Maandelijkse verdienste: €")
-		fmt.Scanln(&maandloon)
-		fmt.Print("Aantal jaren: ")
-		fmt.Scanln(&jaar)
-		fmt.Print("Aantal maanden: ")
-		fmt.Scanln(&maand)
-		maand += (12 * jaar)
-		maandsparen = ((spaargeld + gepland - rechtstreeks) / maand)
-		fmt.Print("\nMaandelijks moet je €")
-		fmt.Printf("%.2f", maandsparen)
-		fmt.Print(" sparen")
-		if spaargeld > gepland {
-			fmt.Print("\nWaarvan €")
-			fmt.Printf("%.2f", gepland/maand)
-			fmt.Print(" voor geplande aankopen")
-			fmt.Print("\nWaarvan €")
-			fmt.Printf("%.2f", (spaargeld-rechtstreeks)/maand)
-			fmt.Print(" voor spaargeld")
-		} else {
-			fmt.Print("\nWaarvan €")
-			fmt.Printf("%.2f", ((gepland - rechtstreeks) / maand))
-			fmt.Print(" voor geplande aankopen")
-			fmt.Print("\nWaarvan €")
-			fmt.Printf("%.2f", (spaargeld / maand))
-			fmt.Print(" voor spaargeld")
-		}
-		fmt.Print("\nJe hebt €")
-		fmt.Printf("%.2f", maandloon-leefgeld-maandsparen)
-		fmt.Print(" over voor maandelijkse fun")
-	}
+    for {
+        var monthlyIncome, otherAnnualIncome, fixedMonthlyCosts, plannedAnnualExpenses, annualSavings float64
+
+        // User inputs
+        fmt.Print("Maandelijks inkomen: ")
+        fmt.Scanln(&monthlyIncome)
+
+        fmt.Print("Extra jaarlijks inkomen: ")
+        fmt.Scanln(&otherAnnualIncome)
+
+        fmt.Print("Vaste verplichte maandelijkse kosten: ")
+        fmt.Scanln(&fixedMonthlyCosts)
+
+        fmt.Print("Geplande jaarlijkse uitgaven (b.v., reizen, geplande aankopen): ")
+        fmt.Scanln(&plannedAnnualExpenses)
+
+        fmt.Print("Jaarlijks doel om te sparen: ")
+        fmt.Scanln(&annualSavings)
+
+        // Aanpassingen
+        annualSavingsRequired := annualSavings - otherAnnualIncome
+        if annualSavingsRequired < 0 {
+            plannedAnnualExpenses += -annualSavingsRequired
+            annualSavingsRequired = 0
+        }
+
+        // Calculations
+        totalAnnualIncome := (monthlyIncome * 12)
+        totalPlannedAnnualExpenses := plannedAnnualExpenses
+        totalAnnualSavings := annualSavingsRequired
+        totalExpenses := (fixedMonthlyCosts * 12) + totalPlannedAnnualExpenses
+        monthlyFunBudget := (totalAnnualIncome - totalExpenses - totalAnnualSavings) / 12
+
+        // Output
+        fmt.Printf("\nMaandelijks naar Fun Rekening: €%.2f\n", monthlyFunBudget)
+        fmt.Printf("Maandelijks laten staan op Zichtrekening (Vaste verplichte kosten): €%.2f\n", fixedMonthlyCosts)
+        fmt.Printf("Maandelijks naar Geplande Spaarrekening: €%.2f\n", plannedAnnualExpenses/12)
+        fmt.Printf("Maandelijks naar Lange Termijn Spaarrekening: €%.2f\n", totalAnnualSavings/12)
+        fmt.Printf("Jaarlijkse bedrag extra sparen op Lange Termijn Spaarrekening: €%.2f\n", totalAnnualSavings)
+        fmt.Println("\n\n")
+    }
 }
